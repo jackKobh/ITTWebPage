bc42822ff5be0cad905b4e69e66f0d186a44346a
Urbano Ballesteros Rodrigue <urbanoballesteros@gmail.com>
2016-10-04 00:14:11 -0500

    REDES Y BASE
    Se añade funcionalidad para copartir noticias en redes sociales
    (Facebook, twitter y g+), ademas de crear definicion de tabla de
    configuracion de noticias (NOTICE_CONF)

--- js/accionesPage.js
+++ js/accionesPage.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 	ejecutaPeticionHTML('Framework/Acciones/muestraSliderBotones.php',{}, 'btn-slider-principal');
 	ejecutaPeticionHTML('Framework/Acciones/muestraSliderImagenes.php',{}, 'img-slider-principal');
 
-	$('#btn-show-pdf').on('click',function(){
+	/*$('#btn-show-pdf').on('click',function(){
         var pdf_link = 'files/prueba2.pdf';
         //alert(pdf_link);
         //var iframe = '<div class="iframe-container"><iframe src="'+pdf_link+'"></iframe></div>'
@@ -13,6 +13,59 @@ $(document).ready(function(){
         $('.modal-body').html(iframe);
 
         $('#myModal').modal('toggle');
-    });
+    });*/
 });
 
+function muestraPDF(url, titulo)
+{
+    //
+    //var pdf_link = 'files/prueba2.pdf';
+    var iframe = '<object type="application/pdf" data="'+url+'" width="100%" height="500">No Support</object>'
+    //alert(iframe);    
+    $('#itm-tittle-pdf').text(titulo);
+    $('#modal-body-pdf').html(iframe);
+    $('#myModal-pdf').modal('toggle');
+}
+
+(function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+
+!function(d,s,id){
+    var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';
+    if(!d.getElementById(id)){
+        js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';
+        fjs.parentNode.insertBefore(js,fjs);
+    }
+}(document, 'script', 'twitter-wjs');
+
+
+function compartirFB(url)
+{
+    window.open('https://www.facebook.com/sharer/sharer.php?u='+
+                encodeURIComponent(url),
+                'facebookshare','width=540,height=480,resizable=yes'); 
+    return false;
+}
+
+
+function compartirTW(url,text)
+{
+    window.open('https://twitter.com/intent/tweet?original_referer=' +
+                encodeURIComponent(url)+
+                '&ref_src=twsrc%5Etfw&text=' + text + '&tw_p=tweetbutton&url='+
+                encodeURIComponent(url),
+                'twittershare','width=540,height=480,resizable=yes');
+    return false;
+}
+
+function compartirGP(url)
+{
+    window.open('https://plus.google.com/share?url='+encodeURIComponent(url),
+        'googleshare','width=540,height=480,resizable=yes');
+    return false;
+}
